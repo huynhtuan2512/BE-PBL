666ef25a228d820f9cf164485f887c3a6d41c01f
huynhtuan2512 <hdinhtuan93@gmail.com>
2026-05-13 22:37:20 +0700

    m

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -1,3 +1,19 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class UserCreate(BaseModel):
+    username: str
+    password: str
+    full_name: Optional[str] = None
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = 'bearer'
+
 
 
     class Config:
